23676adce80be6b24945f782b2afdbbd944828d2
Justin Seymour <justin@3wmholdings.com>
2017-12-14 10:13:54 +0200

    Initial Commit

--- jsonExercise/ViewController.swift
+++ jsonExercise/ViewController.swift
@@ -10,15 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    struct Course: Decodable {
+        let id: Int
+        let name: String
+        let link: String
+        let imageUrl: String
+        
+//        init(json: [String: Any]) {
+//            id = json["id"] as? Int ?? -1
+//            name = json["name"] as? String ?? ""
+//            link = json["link"] as? String ?? ""
+//            imageUrl = json["imageUrl"] as? String ?? ""
+//        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let jsonUrlString = "https://api.letsbuildthatapp.com/jsondecodable/course"
+        guard let url = URL(string: jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            
+            guard let data = data else { return }
+            
+            
+            do {
+                let course = try JSONDecoder().decode(Course.self, from: data)
+                print(course.name)
+            } catch let jsonErr {
+                
+            }
+            
+            
+            
+            // ** Store data response from completion handler as a string and print it out to the console ** //
+            // ================================================================================================
+            
+            // let dataAsString = String(data: data, encoding: .utf8)
+            // print(dataAsString as Any)
+            
+            
+            // ** TODO: comment ** //
+            // ================================================================================================
+            //            do {
+            //                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
+            //
+            //                let course = Course(json: json)
+            //                print(course.name)
+            //
+            //            } catch let jsonErr {
+            //                print("Error printing json")
+            //            }
+            
+            
+        }.resume()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
